651c4a8a240f1172077f7dfff398f37019277e7b
Umarley Ricardo <umarleyricardo@gmail.com>
2021-05-05 19:25:51 -0300

    ajustes

--- module/Application/src/Controller/RespostasController.php
+++ module/Application/src/Controller/RespostasController.php
@@ -9,15 +9,24 @@ use Laminas\View\Model\ViewModel;
 
 class RespostasController extends Abstracao\API {
 
-    public function estadoAction() {
-        $codigoEstado = $this->params()->fromRoute('id');
-        if (empty($codigoEstado)) {
-            $this->populaResposta(404, ['messages' => "Código do estado deve ser informado!"], false);
-        } else {
-            $modelMunicipios = new \Application\Model\MunicipiosModel();
-            $this->populaResposta(200, $modelMunicipios->getAll($codigoEstado));
-        }
+    public function indexAction() {
+
+            $conteudo = file_get_contents("php://input");
+            $arPost = json_decode($conteudo, true);
+
+            var_dump($arPost);
+        
         exit;
     }
 
+    /* public function estadoAction() {
+      $codigoEstado = $this->params()->fromRoute('id');
+      if (empty($codigoEstado)) {
+      $this->populaResposta(404, ['messages' => "Código do estado deve ser informado!"], false);
+      } else {
+      $modelMunicipios = new \Application\Model\MunicipiosModel();
+      $this->populaResposta(200, $modelMunicipios->getAll($codigoEstado));
+      }
+      exit;
+      } */
 }
