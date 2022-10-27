b1969a5f21841fe0f69391c8d79c57282fe839d7
Umarley Ricardo <umarleyricardo@gmail.com>
2022-10-27 09:27:21 -0300

    carga de dados questionarios

--- module/Application/src/Entity/Console/RespostasQuestoes.php
+++ module/Application/src/Entity/Console/RespostasQuestoes.php
@@ -10,7 +10,7 @@ use Laminas\Db\Sql\Predicate\Expression;
 class RespostasQuestoes extends AbstractDatabase {
 
     public function __construct() {
-        $this->table = 'respostas_q1';
+        $this->table = 'respostas_q2';
         $this->primaryKey = 'rowId';
         parent::__construct(AbstractDatabase::DATABASE_CORE);
     }
